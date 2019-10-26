6e6255237bb77590b36b941ab4f0d42430e8a750
Gurashish Singh <gurashish1singh@gmail.com>
2019-10-25 21:40:35 -0400

    Added posts using shell and made changes to the date_posted in home.html

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Pooja Noob',
-        'title': 'Blog Post 1',
-        'content': 'I am such a noob',
-        'date_posted': 'October 24,2019'
-    },
+# Hard coding data
+# posts = [
+#     {
+#         'author': 'Pooja Noob',
+#         'title': 'Blog Post 1',
+#         'content': 'I am such a noob',
+#         'date_posted': 'October 24,2019'
+#     },
 
-        {
-        'author': 'Gushu',
-        'title': 'Blog Post 2',
-        'content': 'The post above is totally true!',
-        'date_posted': 'October 25,2019'
-    }
-]
-def home(request,):
+#         {
+#         'author': 'Gushu',
+#         'title': 'Blog Post 2',
+#         'content': 'The post above is totally true!',
+#         'date_posted': 'October 25,2019'
+#     }
+# ]
+def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
